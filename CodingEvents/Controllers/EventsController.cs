544df2bac64a69710eebd08091680df902078885
Cory <corydeuschle92@gmail.com>
2024-09-03 11:35:58 -0500

    commit before adding db

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,86 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using CodingEvents.ViewModels;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Event> events = new List<Event>(EventData.GetAll());
+
+            return View(events);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+
+            return View(addEventViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                    Type = addEventViewModel.Type,
+                };
+
+                EventData.Add(newEvent);
+
+                return Redirect("/Events");
+            }
+           return View(addEventViewModel);
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds) 
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Edit(int eventId)
+        {
+            Event eventToEdit = EventData.GetById(eventId);
+            ViewBag.eventToEdit = eventToEdit;
+            ViewBag.title = "Edit Event" + eventToEdit.Name + "(id = " + eventToEdit.Id + ")";
+
+            return View();
+
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit{eventId}")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event eventToEdit = EventData.GetById(eventId);
+            eventToEdit.Name = name;
+            eventToEdit.Description = description;
+
+           return Redirect("/Events");
+        }
+    }
+}
---
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage ="Name for event is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Event name must be between 3 and 50 characters")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Description of event is required")]
+        [StringLength(500, ErrorMessage ="Description is too long")]
+        public string? Description { get; set; }
+
+        [EmailAddress]
+        public string? ContactEmail { get; set; }
+
+        public EventType Type { get; set; }
+
+        public List<SelectListItem> EventTypes { get; set; } = new List<SelectListItem>
+        {
+              new SelectListItem(EventType.Conference.ToString(), ((int)EventType.Conference).ToString()),
+              new SelectListItem(EventType.Meetup.ToString(), ((int)EventType.Meetup).ToString()),
+              new SelectListItem(EventType.Social.ToString(), ((int)EventType.Social).ToString()),
+              new SelectListItem(EventType.Workshop.ToString(), ((int)EventType.Workshop).ToString())
+        };
+    }
+}
